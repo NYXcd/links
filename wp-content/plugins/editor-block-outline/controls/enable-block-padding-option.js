a6077ebc9f85ca58504a3f12e9c132cd9442d742
NYXcd <nyxcybirdottir@gmail.com>
2026-06-25 13:02:56 +1000

    Uploaded links version 2.

---
+++ wp-content/plugins/editor-block-outline/controls/enable-block-padding-option.js
@@ -0,0 +1,35 @@
+function enableOutlinePaddingOption(props) {
+	const { ToggleControl } = this.components;
+
+	let currentValue = props.metaValue;
+	const enableOutlinePaddingValue = props.outlinePaddingEnableState;
+
+	if (currentValue === undefined) {
+		currentValue = true;
+	}
+
+	// if state value is set then use it. This will disable UI flickering
+	if (enableOutlinePaddingValue !== undefined && enableOutlinePaddingValue !== '') {
+		currentValue = enableOutlinePaddingValue;
+	}
+
+	return this.el(ToggleControl, {
+		label: 'Enable outline padding',
+		checked: currentValue,
+		onChange: (value) => {
+			// update state to update control ui
+			props.setState({
+				outlinePaddingEnableState: value,
+			});
+
+			// update editor ui
+			const body = getDocumentBody();
+			body.attr('enable-outline-padding', value);
+
+			// update meta
+			this.debounce(() => {
+				props.setMetaValue(value);
+			})();
+		},
+	});
+}
